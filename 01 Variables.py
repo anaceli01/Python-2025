3730811dfbf664ad8f288b13519359891fd47d3d
karla Miranda <karlaanaceli.miranda@alumnos.ulagos.cl>
2025-05-07 18:03:03 -0400

    Apuntes Variables

---
+++ 01 Variables.py
@@ -0,0 +1,20 @@
+"""Guía inicial de Python (Variables)"""
+
+nombre = "Karla"
+apellido = "Miranda"
+
+#Iniciando variablesen un print
+
+#Con coma
+print ("Hola, mi nombr es ", nombre, " y mi apellido es", apellido)
+
+#Con signo +
+print ("Hola, mi nombre es " + nombre + " y mi apellido es " + apellido)
+
+#Con llaves
+print (f"Hola, mi nombre es {nombre} y mi apellido es {apellido}")
+
+#Variables
+ciudad, region, pais = "Castro", "Los lagos", "Chile"
+
+print (f"Hola soy de {ciudad}, {region}, {pais}")
