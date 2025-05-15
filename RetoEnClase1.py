a6e102b1875dc84d7b8713c60873d5a67e5ad543
karla Miranda <karlaanaceli.miranda@alumnos.ulagos.cl>
2025-05-15 16:57:44 -0400

    Apuntes completos clase 15-05

---
+++ RetoEnClase1.py
@@ -0,0 +1,9 @@
+
+
+#Variables
+
+nombre = input ("Ingrese su nombre:")
+edad = int(input ("Ahora ingrese su edad:"))
+total_edad = edad + 20
+
+print (f"Hola, mi nombre es {nombre}, tengo {edad} años y en veinte años tendré {total_edad} años")
