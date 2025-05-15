2e0406e1f36b604cddb742409c37928c96b07d79
karla Miranda <karlaanaceli.miranda@alumnos.ulagos.cl>
2025-05-15 15:30:09 -0400

    clase 15-05

---
+++ 02 Tiposdedatos.py
@@ -0,0 +1,14 @@
+
+#DECLARANDO E INICIALIZANDO VARIABLES NÚMERICAS
+edad = 31
+estatura = 1.71
+peso = 75.4
+complejo = 1 + 9j
+complejo2 = complex(1, 4)
+
+print (complejo)
+print (complejo2)
+
+#OPERACIÓN ARITMÉTICA
+imc = peso / (estatura ** 2)
+print (f"Su IMC es: {imc}")
